0156d78c3fc18dd04d4d2b34f9dcacfff5c26693
hainj <jakub.hain@gmail.com>
2013-10-30 11:37:40 +0100

    Update cteni.java
    luxus

--- PT_Seminarka/src/pack/cteni.java
+++ PT_Seminarka/src/pack/cteni.java
@@ -1,3 +1,7 @@
+                                                                     
+                                                                     
+                                                                     
+                                             
 package pack;
 
 import java.io.BufferedReader;
