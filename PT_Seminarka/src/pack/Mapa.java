6ab8612f50c3816b4de255f3148dbedc2e2b4a2b
Cybra <Cybra@Jakub>
2013-12-03 13:27:39 +0100

    whoops

--- PT_Seminarka/src/pack/Mapa.java
+++ PT_Seminarka/src/pack/Mapa.java
@@ -1,222 +1,196 @@
 
+
+
+
 package pack;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
-import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Mesto {
+public class Mapa extends JPanel {
 
-	private int jidlo = 0;
-	private int jidlaTreba = 0;
-	private int obyvatel = 0;
-	private int x = 0;
-	private int y = 0;
-	private ArrayList<Mesto> sousedi;
-	private boolean heliport;
-	private double vzdalenost = 100000;
-	private Mesto predchudce;
-	private Letiste odkud;
-	private boolean hlad = true;
-	private boolean maCesty = true;
-
-
-
-
+	public static ArrayList<Mesto>poleMest = new ArrayList<Mesto>();
+	public static int width;
+	public static int height;
+	public static ArrayList<Letiste>poleLetist = new ArrayList<Letiste>();
+	static Boolean ovladani = false;
 
 	/**
-	 * Konstruktor mest pro jeho polohu a pocet obyvatel
-	 * @param x x-ova souradnice
-	 * @param y y-ova souradnice
-	 * @param obyvatel pocet obyv
+	 * Konstruktor tridy mapa 
+	 * @param sirka
+	 * @param vyska
 	 */
-	public Mesto(int x, int y, int obyvatel, boolean heli)
+	 public Mapa (int sirka, int vyska, Boolean ovladani)
 	{
-		this.x = x;
-		this.y = y;
-		this.obyvatel = obyvatel;
-		this.heliport = heli;
-		//mesto potrebuje 2kg jidla na obyv na den - je mozno skladovat 3 dny
-		this.jidlaTreba = obyvatel*2*3;
+		this.setSize(sirka, vyska);
+		this.setPreferredSize(this.getSize());
+		this.width = sirka;
+		this.height = vyska;
+		
+		this.ovladani = ovladani;
 	}
+	 /**
+	  * Vraci pole letist
+	  * @return pole letist
+	  */
+	 public static ArrayList<Letiste> getPoleLetist() {
+		 return poleLetist;
+	 }
 
-	/**
-	 *  Konstruktor mesta s polohou, poctem obyvatel a jeho sousedy
-	 * @param x x-ova souradnice
-	 * @param y y-ova souradnice
-	 * @param obyvatel pocet obyv
-	 * @param sousedi pole sousedu mesta
-	 */
-	public Mesto( int x, int y, int obyvatel,ArrayList<Mesto> sousedi) {
-		super();
-		this.obyvatel = obyvatel;
-		this.x = x;
-		this.y = y;
-		this.sousedi = sousedi;
-		//mesto potrebuje 2kg jidla na obyv na den - je mozno skladovat 3 dny
-		this.jidlaTreba = obyvatel*2*3;
-	}
+	 /**
+	  * Prida mesto do seznamu poli
+	  * @param mesto
+	  */
+	 public static void addPoleMest(Mesto mesto) {
+		 poleMest.add(mesto);
+	 }
 
+	 /**
+	  * vraci index mesta v poliMest
+	  * @param mest ktereho index zjistuji
+	  * @return index mesta v poli
+	  */
+	 public static int getIndexMest(Mesto mest){
 
+		 for(int i = 0; i<poleMest.size(); i++){
+			 if(mest.equals(poleMest.get(i))){
 
-	/**
-	 * @return the jidlo
-	 */
-	public int getJidlo() {
-		return jidlo;
-	}
+				 return i;
+			 }
+		 }
+		 return -1;
+	 }
 
-	/**
-	 * @param jidlo the jidlo to set
-	 */
-	public void setJidlo(int jidlo) {
-		this.jidlo += jidlo;
-	}
+	 public static ArrayList<Mesto> getPoleMest() {
+		 return poleMest;
+	 }
 
-	/**
-	 * @return the jidlaTreba
-	 */
-	public int getJidlaTreba() {
-		return jidlaTreba;
-	}
+	 public void setMesta(ArrayList<Mesto> poleMest)
+	 {
+		 this.poleMest = poleMest;
+	 }
 
-	/**
-	 * @param jidlaTreba the jidlaTreba to set
-	 */
-	public void setJidlaTreba(int jidlaTreba) {
-		this.jidlaTreba = jidlaTreba;
-	}
+	 public static void setLetiste(ArrayList<Letiste> poleLetiste)
+	 {
+		 poleLetist = poleLetiste;
+	 }
 
-	/**
-	 * @return the maCesty
-	 */
-	public boolean isMaCesty() {
-		return maCesty;
-	}
+	 /**
+	  * zakladni kreslici metoda
+	  */
+	 public void paint (Graphics g)
+	 {
+		 super.paint(g);
+		 Graphics2D g2 = (Graphics2D) g;
+		 kresliMapu(g2, new Rectangle(0, 0, width, height));
+	 }
 
-	/**
-	 * @param maCesty the maCesty to set
-	 */
-	public void setMaCesty(boolean maCesty) {
-		this.maCesty = maCesty;
-	}
+	 /**
+	  * Dale rozvedena kreslici metoda, kresli pomoci Graphics2D
+	  * @param g2
+	  * @param drawingArea
+	  */
+	 public void kresliMapu(Graphics2D g2, Rectangle drawingArea)
+	 {
+		 // vymazani platna
+		 g2.setColor(Color.WHITE);
+		 g2.fillRect(0, 0, width, height);
 
-	/**
-	 * @return the vzdalenost
-	 */
-	public double getVzdalenost() {
-		return vzdalenost;
-	}
+		 g2.setStroke(new BasicStroke(2));
 
-	/**
-	 * @param vzdalenost the vzdalenost to set
-	 */
-	public void setVzdalenost(double vzdalenost) {
-		this.vzdalenost = vzdalenost;
-	}
+		 //vykresleni centralni oblasti
+		 g2.setColor(Color.GREEN);
+		 g2.drawRect(225, 225, 50, 50);
 
-	/**
-	 * @return the predchudce
-	 */
-	public Mesto getPredchudce() {
-		return predchudce;
-	}
+		 g2.setColor(Color.BLACK);
 
-	/**
-	 * @param predchudce the predchudce to set
-	 */
-	public void setPredchudce(Mesto predchudce) {
-		this.predchudce = predchudce;
-	}
+		 //g2.drawRect(502, 0, 700, 550);
 
-	/**
-	 * @return the odkud
-	 */
-	public Letiste getOdkud() {
-		return odkud;
-	}
+		 //ovladaci promenna - true program uz bezi a ma mapu nebo se spustil a je prazdny
+		 if(this.ovladani == true)
+		 {
+			 //jeli spusteny program prazdny vygeneruje novou mapu
+			 if(poleMest.size()==0){
+				new Generator();
+			 }
 
-	/**
-	 * @param odkud the odkud to set
-	 */
-	public void setOdkud(Letiste odkud) {
-		this.odkud = odkud;
-	}
+			 kresleni(g2);
+		 }
+		 //ovladaci promenna - false pri spusteni nacte predpripravenou mapu
+		 else
+		 {
+			 cteni.vstupMapa(new File("ZakladniMapa.txt"));
+			 new Generator(poleMest, poleLetist);
+			 
+			 kresleni(g2);
+		 }
 
-	/**
-	 * Vraci boolean, zda mesto ma nebo nema heliport
-	 * @return
-	 */
-	public boolean getHeliport() {
-		return heliport;
-	}
+	 }
 
-	public void setHeliport(boolean heliport) {
-		this.heliport = heliport;
-	}
+	 /**
+	  * metoda vykreslujici mesta letiste a cesty
+	  * zabranuje opakovani kodu
+	  * @param g2
+	  */
+	 private static void kresleni(Graphics2D g2)
+	 {
+		 //vykresleni cest mezi mesty
+		 g2.setStroke(new BasicStroke(1));
+		 g2.setColor(Color.BLUE);
+		 Rectangle rect = new Rectangle(501, 0,100 ,500);
 
-	public ArrayList<Mesto> getSousedi() {
-		return sousedi;
-	}
+		 int indexMestPod2 = Generator.zjistiPod2(poleMest);
 
-	public void setSousedi(ArrayList<Mesto> sousedi) {
-		this.sousedi = sousedi;
-	}
+		 for(int i = indexMestPod2+1; i <3000; i++)
+		 {
+			 for (int j = 0; j <10; j++)
+			 {
+				 g2.drawLine(poleMest.get(i).getX(), poleMest.get(i).getY(),
+						 poleMest.get(i).getSousedi().get(j).getX(), 
+						 poleMest.get(i).getSousedi().get(j).getY());
+			 }
+		 }
 
-	public int getObyvatel() {
-		return obyvatel;
-	}
+		 //kresleni cest z letist
+		 g2.setStroke(new BasicStroke(1));
+		 g2.setColor(Color.RED);
 
-	public void setObyvatel(int obyvatel) {
-		this.obyvatel = obyvatel;
-	}
+		 for(int i = 0; i <poleLetist.size(); i++)
+		 {
+			 //	System.out.println("ahoj " + poleLetist.get(i).getSousedi().size());
+			 for (int j = 0; j <60; j++)
+			 {	
+				 //System.out.println(j);
+				 g2.drawLine(poleLetist.get(i).getX(), poleLetist.get(i).getY(),
+						 poleLetist.get(i).getSousedi().get(j).getX(), 
+						 poleLetist.get(i).getSousedi().get(j).getY());
+			 }
+		 }
 
-	public int getX() {
-		return x;
-	}
+		 //kresleni mest
+		 g2.setColor(Color.BLACK);
+		 for(int i = 0; i<poleMest.size()-1; i++)
+		 {
+			 g2.fillRect(poleMest.get(i).getX()-1, poleMest.get(i).getY()-1, 3, 3);
+		 }
+		 //kresleni letist
+		 g2.setColor(Color.RED);
 
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.x = y;
-	}
-
-	/**
-	 * @return the hlad
-	 */
-	public boolean getHlad() {
-		return hlad;
-	}
-
-	/**
-	 * @param hlad the hlad to set
-	 */
-	public void setHlad(boolean hlad) {
-		this.hlad = hlad;
-	}
-
-	@Override
-	public String toString() {
-		String str = "";
-		if(this.isMaCesty()){
-			str = "Jidlo: " + this.jidlo + "\n" + 
-					"Jidlo treba: " + this.getJidlaTreba() + "\n" + 
-					"Hlad: " + this.getHlad() + "\n" + 
-					"Vzdalenost: " + this.getVzdalenost();
-		}
-		else{
-			str = "Jidlo: " + this.jidlo + "\n" +  
-					"Jidlo treba: " + this.getJidlaTreba() + "\n" + 
-					"Hlad: " + this.getHlad() + "\n" + 
-					"Vzdalenost: od mesta s heliportem " + this.getVzdalenost()+ "\n" +
-					"Vzdalenost od letiste: " + (this.getVzdalenost() + this.getPredchudce().getVzdalenost());
-					
-		}
-
-		return str;
-	}
+		 for(int i = 0; i<poleLetist.size(); i++)
+		 {
+			 g2.setColor(Color.RED);
+			 g2.fillRect(poleLetist.get(i).getX()-2, poleLetist.get(i).getY()-2, 5, 5);
+			 g2.setColor(Color.BLACK);
+			 g2.drawRect(poleLetist.get(i).getX()-2, poleLetist.get(i).getY()-2, 5, 5);
+		 }
+	 }
 }
