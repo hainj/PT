ca36434d0a1a41ec8309d5b017b1b42006a9146c
hainj <jakub.hain@gmail.com>
2013-12-03 13:20:21 +0100

    Update cteni.java
    fdfdfd

--- PT_Seminarka/src/pack/cteni.java
+++ PT_Seminarka/src/pack/cteni.java
@@ -21,193 +21,196 @@ public class cteni {
 	/**
 	 * Vypise mesta a letiste do souboru
 	 */
-	 public static void vystupMapa(ArrayList<Mesto> poleMest, ArrayList<Letiste> let, File f, int bezSous) {
-		 // TODO Auto-generated method stub
+	public static void vystupMapa(ArrayList<Mesto> poleMest, ArrayList<Letiste> let, File f, int bezSous) {
+		// TODO Auto-generated method stub
 
-		 FileOutputStream stream;
-		 try {
-			 stream = new FileOutputStream(f);
-			 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stream));
+		FileOutputStream stream;
+		try {
+			stream = new FileOutputStream(f);
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stream));
 
-			 writer.append(String.valueOf(bezSous));
-			 writer.newLine();
-			 //vypise x, y a jidlo  letiste
-			 for(int i = 0; i<let.size();i++){
-				 writer.append(let.get(i).getX() + " " +  let.get(i).getY() + " " + let.get(i).getJidlo());
-				 writer.newLine();
-			 }
-			 //vypise x, y, pocet obyv mesta
-			 for(int i = 0; i<poleMest.size();i++){
-				 Mesto mesto = poleMest.get(i);
+			writer.append(String.valueOf(bezSous));
+			writer.newLine();
+			//vypise x, y a jidlo  letiste
+			for(int i = 0; i<let.size();i++){
+				writer.append(let.get(i).getX() + " " +  let.get(i).getY() + " " + let.get(i).getJidlo());
+				writer.newLine();
+			}
+			//vypise x, y, pocet obyv mesta
+			for(int i = 0; i<poleMest.size();i++){
+				Mesto mesto = poleMest.get(i);
 
-				 writer.append(mesto.getX() + " " +  mesto.getY() + " " + mesto.getObyvatel() + " " + mesto.getHeliport());
-				 writer.newLine();
-			 }
-			 writer.append("sousedi letist");
+				writer.append(mesto.getX() + " " +  mesto.getY() + " " + mesto.getObyvatel() + " " + mesto.getHeliport());
+				writer.newLine();
+			}
+			writer.append("sousedi letist");
 
-			 writer.newLine();
+			writer.newLine();
 
-			 //vypise sousedy letist
-			 for (int j = 0; j<let.size(); j++){
+			//vypise sousedy letist
+			for (int j = 0; j<let.size(); j++){
 
-				 String str = "";
-				 for(int z = 0; z<let.get(j).getSousedi().size();z++){
-					 str = str + Mapa.getIndexMest(let.get(j).getSousedi().get(z)) + " ";
+				String str = "";
+				for(int z = 0; z<let.get(j).getSousedi().size();z++){
+					str = str + Mapa.getIndexMest(let.get(j).getSousedi().get(z)) + " ";
 
-				 }
-				 writer.append(str);
-				 writer.newLine();
+				}
+				writer.append(str);
+				writer.newLine();
 
-			 }
+			}
 
-			 writer.write("sousedi mest");
-			 writer.newLine();
+			writer.write("sousedi mest");
+			writer.newLine();
 
-			 //vypise sousedy mest
-			 for(int i =  Generator.indexMestPod2+1; i<poleMest.size();i++){
-				 Mesto mesto = poleMest.get(i);
-				 String str = "";
-				 //System.out.println(i);
-				 try {
-					 int pocetSous = mesto.getSousedi().size();
-					 //System.out.println(pocetSous);
-					 for (int j =0; j<pocetSous;j++){
-						 str = str + Mapa.getIndexMest(mesto.getSousedi().get(j)) + " ";	
+			//vypise sousedy mest
+			for(int i =  Generator.indexMestPod2+1; i<poleMest.size();i++){
+				Mesto mesto = poleMest.get(i);
+				String str = "";
+				//System.out.println(i);
+				try {
+					int pocetSous = mesto.getSousedi().size();
+					//System.out.println(pocetSous);
+					for (int j =0; j<pocetSous;j++){
+						str = str + Mapa.getIndexMest(mesto.getSousedi().get(j)) + " ";	
 
-					 }
-					 writer.append(str);
-					 writer.newLine();
-				 } catch (Exception e) {
+					}
+					writer.append(str);
+					writer.newLine();
+				} catch (Exception e) {
 
-				 }
+				}
 
-			 }
-			 writer.append("exit");
-			 writer.close();
-			 stream.close();
-		 } catch (FileNotFoundException e) {
-			 // TODO Auto-generated catch block
-			 e.printStackTrace();
-		 } catch (IOException e) {
-			 // TODO Auto-generated catch block
-			 e.printStackTrace();
-		 }
-	 }
-	 /**
-	  * Nacte letiste a mesta ze vstupniho souboru
-	  * @param f vstupni soubor
-	  */
-	 static void vstupMapa(File f) {
-		 // TODO Auto-generated method stub
+			}
+			writer.append("exit");
+			writer.close();
+			stream.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * Nacte letiste a mesta ze vstupniho souboru
+	 * @param f vstupni soubor
+	 */
+	static void vstupMapa(File f) {
+		// TODO Auto-generated method stub
 
-		 FileInputStream stream;
-		 try {
-			 stream = new FileInputStream(f);
-			 BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-			 String str;
-			 Mapa.getPoleLetist().clear();
-
-
-			 int a = Integer.valueOf(reader.readLine());
-			 Generator.indexMestPod2 = a;
-			 //nacte letiste
-			 for(int i = 0; i<5;i++){
-				 str = reader.readLine();
-
-				 String[]s = str.split(" ");
+		FileInputStream stream;
+		try {
+			stream = new FileInputStream(f);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+			String str;
+			Mapa.getPoleLetist().clear();
 
 
-				 Mapa.getPoleLetist().add(new Letiste(Integer.parseInt(s[0]), Integer.parseInt(s[1])));
+			int a = Integer.valueOf(reader.readLine());
+			Generator.indexMestPod2 = a;
+			//nacte letiste
+			for(int i = 0; i<5;i++){
+				str = reader.readLine();
+
+				String[]s = str.split(" ");
 
 
-			 }	
-			 //Mapa.setLetiste(letist);
-			 str = reader.readLine();
-			 Mapa.getPoleMest().clear();
-			 //nacte mesta
-			 while(!str.equalsIgnoreCase("sousedi letist")){
+				Mapa.getPoleLetist().add(new Letiste(Integer.parseInt(s[0]), Integer.parseInt(s[1])));
 
 
-				 String[]s = str.split(" ");
+			}	
+			//Mapa.setLetiste(letist);
+			str = reader.readLine();
+			Mapa.getPoleMest().clear();
+			//nacte mesta
 
-				 Mapa.addPoleMest(new Mesto(Integer.parseInt(s[0]),Integer.parseInt(s[1]),Integer.parseInt(s[2]),Boolean.parseBoolean(s[3])));
-				 str = reader.readLine();
-			 }
-
-
-			 //sousedi letist
-			 str = reader.readLine();
-			 int g =0;
-			 while(!str.equalsIgnoreCase("sousedi mest")){
-
-				 ArrayList<Mesto> pomoc = new ArrayList<Mesto>();
-				 String[]s = str.split(" ");
-
-				 for (int i = 0; i < s.length;i++){
-					 int k = Integer.parseInt(s[i]);
-					 pomoc.add(Mapa.getPoleMest().get(k));
-				 }
-
-				 Mapa.getPoleLetist().get(g).setSousedi(pomoc);
-				 str = reader.readLine();
-
-				 g++;
-			 }
+			while(!str.equalsIgnoreCase("sousedi letist")){
 
 
+				String[]s = str.split(" ");
+
+				Mapa.addPoleMest(new Mesto(Integer.parseInt(s[0]),Integer.parseInt(s[1]),Integer.parseInt(s[2]),Boolean.parseBoolean(s[3])));
+				str = reader.readLine();
+			}
 
 
-			 //sousedi mest
-			 str = reader.readLine();
-			 int z = a+1;
-			 while(!str.equalsIgnoreCase("exit")){
+			//sousedi letist
+			str = reader.readLine();
+			int g =0;
+			while(!str.equalsIgnoreCase("sousedi mest")){
 
+				ArrayList<Mesto> pomoc = new ArrayList<Mesto>();
+				String[]s = str.split(" ");
+				//	 
 
-				 ArrayList<Mesto> pomoc = new ArrayList<Mesto>();
+				System.out.println(s.length);
+				for (int i = 0; i < s.length;i++){
+					int k = Integer.parseInt(s[i]);
+					pomoc.add(Mapa.getPoleMest().get(k));
+				}
 
-				 String[]s = str.split(" ");
-				 for (int i = 0; i<s.length; i++){
-					 int k = Integer.parseInt(s[i]);
-					 pomoc.add(Mapa.getPoleMest().get(k));
+				Mapa.getPoleLetist().get(g).setSousedi(pomoc);
 
-				 }
-				 Mapa.getPoleMest().get(z).setSousedi(pomoc);
-				 str = reader.readLine();
+				str = reader.readLine();
 
-				 z++;
-			 }
-			 z=a+1;
-			 Generator.vytvorMatici(Mapa.getPoleMest(), Mapa.getPoleLetist(), z);
-			 for(int i = 0; i<Mapa.getPoleLetist().size();i++){
-				 Generator.dijkstra(Mapa.getPoleLetist().get(i), i);
-			 }
-			 for(int i = 0; i <=a; i++){
-				 Generator.mestoBezSousVzdal(Mapa.getPoleMest().get(i), Mapa.getPoleMest());
-			 }
-			 if(Mapa.getPoleMest().size()>3000){
-				 for(int i = 3000; i <Mapa.getPoleMest().size(); i++){
-					 Generator.mestoBezSousVzdal(Mapa.getPoleMest().get(i), Mapa.getPoleMest());
-				 } 
-			 }
-
-
-
-			 reader.close();
-			 stream.close();
-		 } catch (FileNotFoundException e) {
-			 // TODO Auto-generated catch block
-			 e.printStackTrace();
-		 } catch (IOException e) {
-			 // TODO Auto-generated catch block
-			 e.printStackTrace();
-		 }
+				g++;
+			}
 
 
 
 
+			//sousedi mest
+			str = reader.readLine();
+			int z = a+1;
+			while(!str.equalsIgnoreCase("exit")){
 
 
-	 }
+				ArrayList<Mesto> pomoc = new ArrayList<Mesto>();
+
+				String[]s = str.split(" ");
+				for (int i = 0; i<s.length; i++){
+					int k = Integer.parseInt(s[i]);
+					pomoc.add(Mapa.getPoleMest().get(k));
+
+				}
+				Mapa.getPoleMest().get(z).setSousedi(pomoc);
+				str = reader.readLine();
+
+				z++;
+			}
+			z=a+1;
+			System.out.println(z);
+			Generator.vytvorMatici(Mapa.getPoleMest(), Mapa.getPoleLetist(),z);
+			for(int i = 0; i < Mapa.getPoleLetist().size(); i++){
+				Generator.dijkstra(Mapa.getPoleLetist().get(i), i);
+
+			}
+			if(Mapa.getPoleMest().size()>3000){
+				for(int i = 3000; i < Mapa.getPoleMest().size(); i++){
+					Generator.mestoBezSousVzdal(Mapa.getPoleMest().get(i),Mapa.getPoleMest());
+				}
+			}
+
+
+
+			reader.close();
+			stream.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+
+
+
+	}
 
 
 }
